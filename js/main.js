7bfc563b2d6c634ed64f8e4b9ca2c17cd06c5945
Brandon <brandonrjguthrie@gmail.com>
2022-08-11 13:38:18 -0700

    Add logic for storing array w order of operations
    Use array to store a number input of any size, then record the following clicked operation. Add the number, and operation chose after, store to array, and wipe the number so the process is repeated on user input, increasing array in the order of number, then operation until its ready for calculation.

--- js/main.js
+++ js/main.js
@@ -5,14 +5,67 @@ let div = (x,y) => x/y;
 let operate = (x,y,operator) => operator(x,y);
 
 let numbers = document.querySelectorAll(".num");
-let operators = document.querySelectorAll(".class")
+let operators = document.querySelectorAll(".op")
 let display = document.querySelector("#calcDisplay")
+let currentNum;
+let currentOp;
+let calculations = []
 
 
-//change text content of display when a number is pressed, and store it as a variable
-for (num of numbers){
+//The following code will result in a calculations array with the order of operations
+//ie. [3, add, 2, sub, 1]
+
+
+//number clicked storing logic
+for (let num of numbers){
+    
     num.addEventListener("click", () => {
+
+        //change display to clicked number or concat to the current number
         display.textContent += num.id;
-        firstNum =+ num.id;
+        
+        //if number is undefined, change the current num to the clicked num
+        //else concatonate it to the current num (ex if 5, and clicked 5, current num becomes 55)
+        if (currentNum == undefined){
+            currentNum = num.id;
+        } else {
+        currentNum += num.id;
+        }
+    })
+}
+
+
+//logic for operator being clicked
+for (let op of operators){
+
+    op.addEventListener("click", () => {
+
+        //if there is a number clicked into the calculator
+        if (currentNum !== undefined){
+
+                //add it to the calculations array before the operator
+                calculations.push(currentNum);
+        }
+            
+
+        //if there is no currentNum and there is an operator already selected, this means
+        //that an operator has been selected after another operator with no number input
+        //inbetween. Remove previous operation from array and replace it with the new one
+        //ie if sub operator is clicked and then add operator is clicked [5, add, 3, sub] 
+        //becomes [5, add, 3, add]
+
+        if (currentNum == undefined && currentOp !== undefined){
+            currentOp = op.id;
+            calculations.splice(calculations.length - 1);
+        }
+
+        //wipe current number and display
+        currentNum = undefined;
+        display.textContent = ""
+
+        //push the operator to calculations
+        currentOp = op.id;
+        calculations.push(currentOp);
+        
     })
 }
