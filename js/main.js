91e55ab256ef999f38698cc967f6655ecf39a7ab
Brandon <brandonrjguthrie@gmail.com>
2022-08-11 12:31:47 -0700

    Make function to change display on number click Added an event listener on each number, to store the number as a variable and =+ it to the display on click

--- js/main.js
+++ js/main.js
@@ -2,5 +2,17 @@ let add = (x,y) => x + y;
 let sub = (x,y) => x - y;
 let mult = (x,y) => x*y;
 let div = (x,y) => x/y; 
-
 let operate = (x,y,operator) => operator(x,y);
+
+let numbers = document.querySelectorAll(".num");
+let operators = document.querySelectorAll(".class")
+let display = document.querySelector("#calcDisplay")
+
+
+//change text content of display when a number is pressed, and store it as a variable
+for (num of numbers){
+    num.addEventListener("click", () => {
+        display.textContent += num.id;
+        firstNum =+ num.id;
+    })
+}
